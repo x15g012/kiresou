77cf5a253639a4efe537c5f441b24bd4aa3bb4e8
unknown <x15g012@PC505N025.chiba-fjb.ac.jp>
2015-09-11 11:55:33 +0900

    シュタゲ

--- src/kiresou/version01.java
+++ src/kiresou/version01.java
@@ -3,8 +3,8 @@ package kiresou;
 public class version01 {
 
 	public static void main(String[] args) {
-		System.out.println("えびはら");
-		System.out.println("あべちゃん");
+		System.out.println("歴史改変");
+
 
 	}
 
